bdf18ce161a3e34e79efbe8e5ff1ad8a029b63b3
hafeezrafeeq031 <hafeezrafeeq031@gmail.com>
2025-05-01 23:24:31 -0700

    task is complete

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-function selectColor(div){
-    
-}
+selected = ""
+previousDiv = null; 
+
+function selectColor(div,color) { 
+if (previousDiv != null){ 
+previousDiv.style.borderWidth = "1px" 
+} 
+previousDiv = div 
+div.style.borderWidth = "3px" 
+selected= color }
+
+function handleColor(div) {
+div.style.backgroundColor = selected; 
+div.style.borderWidth = "2px" }
