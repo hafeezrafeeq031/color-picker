a03cef81814f7a7309dbcbd8956b140dada2e525
Hafeez Rafeeq <hafeezrafeeq031@gmail.com>
2025-05-28 21:42:45 +0500

    color picker

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 selected = ""
 previousDiv = null; 
 
-function selectColor(div,color) { 
+function selectColor(box,color) { 
 if (previousDiv != null){ 
 previousDiv.style.borderWidth = "1px" 
 } 
-previousDiv = div 
-div.style.borderWidth = "3px" 
+previousDiv = box 
+box.style.borderWidth = "3px" 
 selected= color }
 
 function handleColor(div) {
 div.style.backgroundColor = selected; 
 div.style.borderWidth = "2px" }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
